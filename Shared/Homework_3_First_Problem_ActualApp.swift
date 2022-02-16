a35e319ea7177df3924ced468c28856487a8f22c
Matthew Malaker <MMalaker@hawk.iit.edu>
2022-02-15 19:55:05 -0600

    Initial Commit

---
+++ Shared/Homework_3_First_Problem_ActualApp.swift
@@ -0,0 +1,17 @@
+//
+//  Homework_3_First_Problem_ActualApp.swift
+//  Shared
+//
+//  Created by Matthew Malaker on 2/15/22.
+//
+
+import SwiftUI
+
+@main
+struct Homework_3_First_Problem_ActualApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
